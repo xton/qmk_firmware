dde2d1c014cc36c1432e15560d0c4ca2a1f8aa1c
Xton <cdewan@apple.com>
2018-06-23 17:36:32 -0700

    WIP - got lower in there

--- keyboards/ergodox_ez/keymaps/xtonhasvim/keymap.c
+++ keyboards/ergodox_ez/keymaps/xtonhasvim/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = LAYOUT_ergodox(  // layer 0 : default
+[_QWERTY] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   X_____X,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   X_____X,
@@ -60,13 +60,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   X_____X,
            X_____X,  X_____X,      KC_LALT, KC_LGUI,  LOWER,
                                               X_____X,  X_____X,
-                                                              X_____X,
-                                               LSFT(KC_LALT),X_____X,X_____X,
+                                                        X_____X,
+                                  LSFT(KC_LALT),X_____X,X_____X,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(_MOVE,KC_SCLN), KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   RSFT_T(KC_ENT),
+             X_____X, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,              KC_BSPC,
+             X_____X, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,              KC_BSPC,
+                      KC_H,   KC_J,   KC_K,   KC_L,   LT(_MOVE,KC_SCLN), KC_QUOT,
+             X_____X, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,           RSFT_T(KC_ENT),
                                   RAISE,  KC_LGUI,KC_LALT,X_____X,  X_____X,
              X_____X, X_____X,
              X_____X,
@@ -77,13 +77,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |    ~   |      |      |      |      |      |      |           |      |      |  _   |   +  |   {  |   }  |  bksp  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
+ * |   del  |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |  BOOM  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+ *   |      |      |      |      |      |                                       |      |    . |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -93,26 +93,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// SYMBOLS
-[SYMB] = LAYOUT_ergodox(
+// LOWER
+[_LOWER] = LAYOUT_ergodox(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
+       X_____X,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  X_____X,
+       KC_TILD,X_____X,X_____X,X_____X,X_____X,X_____X,X_____X,
+       KC_DEL, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,
+       _______,X_____X,X_____X,X_____X,X_____X,X_____X,X_____X,
+       _______,_______,_______,_______,_______,
+                                       X_____X,X_____X,
+                                               X_____X,
+                               X_____X,X_____X,X_____X,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       X_____X, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       X_____X, X_____X, KC_UNDS, KC_PLUS, KC_LCBR,KC_RCBR, KC_BSPC,
+                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,KC_RPRN, KC_PIPE,
+       X_____X, X_____X, X_____X, X_____X, X_____X,X_____X, FIREY_RETURN,
+                         _______, _______, _______,_______, _______,
+       X_____X, X_____X,
+       X_____X,
+       X_____X, X_____X, X_____X
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -157,24 +157,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-      } else {
-        unregister_code(KC_RSFT);
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
