2b53f965a03fec69aa22e8e2b46930e1a7d5a0ee
Xton <cdewan@apple.com>
2018-06-23 17:09:52 -0700

    hide mouse more

--- keyboards/divergetm2/keymaps/xtonhasvim/keymap.c
+++ keyboards/divergetm2/keymaps/xtonhasvim/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,              KC_BSPC, \
   LCTL_T(KC_ESC), LT(_MOVE,KC_A), KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    LT(_MOVE,KC_SCLN), KC_QUOT, \
   KC_LSFT, KC_Z,           KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,   RSFT_T(KC_ENT) , \
-  LSFT(KC_LALT), TG(_MOUSE),  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC, RAISE,   KC_LGUI, KC_LALT, X_____X, VIM_START \
+  LSFT(KC_LALT), X_____X,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC, RAISE,   KC_LGUI, KC_LALT, X_____X, VIM_START \
 ),
 
 /* Lower
@@ -119,12 +119,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RGB_MODE_PLAIN, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
   RGB_MODE_REVERSE, _______, _______,  _______,   _______,  _______, _______, _______, _______, _______, _______, RGB_VAI, \
   RGB_MODE_FORWARD, _______,  _______,  _______,  _______,  _______, _______,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, RGB_VAD, \
-  RGB_TOG, TO(_QWERTY), _______, _______, _______, _______, _______, _______, _______, _______, TO(_QWERTY), X_____X \
+  RGB_TOG, TO(_MOUSE), _______, _______, _______, _______, _______, _______, _______, _______, TO(_QWERTY), X_____X \
 ),
 
 
-/* movement layer (hold semicolon)
- */
+/* movement layer (hold semicolon) */
 [_MOVE] = LAYOUT_ortho_4x12( \
   TO(_QWERTY), X_____X, X_____X, X_____X, X_____X, X_____X, KC_HOME, KC_PGDN, KC_PGUP, KC_END, X_____X, X_____X, \
   _______,     X_____X, LGUI(KC_LBRC), LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC)), LGUI(KC_RBRC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, X_____X, \
