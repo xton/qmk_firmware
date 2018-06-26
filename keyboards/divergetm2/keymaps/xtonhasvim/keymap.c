1b9d1efdbf5dd58c26135b11d7a1eaf6d7a4b04b
Xton <cdewan@apple.com>
2018-06-25 22:26:42 -0700

    sanitized layout a bit
    - mousekeys harder to get to
    - removed fkeys from raise (i tend to hit them by accident too much)
    - specified bootloader so that soft restet works.

--- keyboards/divergetm2/keymaps/xtonhasvim/keymap.c
+++ keyboards/divergetm2/keymaps/xtonhasvim/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,              KC_BSPC, \
-  LCTL_T(KC_ESC), LT(_MOVE,KC_A), KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    LT(_MOVE,KC_SCLN), KC_QUOT, \
+  LCTL_T(KC_ESC), KC_A, KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    LT(_MOVE,KC_SCLN), KC_QUOT, \
   KC_LSFT, KC_Z,           KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,   RSFT_T(KC_ENT) , \
   LSFT(KC_LALT), X_____X,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC, RAISE,   KC_LGUI, KC_LALT, X_____X, VIM_START \
 ),
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_BSPC, \
   KC_DEL, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  X_____X, X_____X, X_____X, X_____X, FIREY_RETURN, \
-  _______, TO(_QWERTY), _______, _______, _______, KC_BSPC, KC_BSPC, _______,    _______,    _______, TO(_QWERTY), X_____X \
+  RESET, TO(_QWERTY), _______, _______, _______, KC_BSPC, KC_BSPC, _______,    _______,    _______, TO(_QWERTY), X_____X \
 ),
 
 /* Raise
@@ -97,10 +97,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSPC, \
+  KC_GRV,  X_____X,   X_____X,   X_____X,   X_____X,   X_____X,   X_____X,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSPC, \
   KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  X_____X, X_____X, X_____X, X_____X, FIREY_RETURN, \
-  _______, TO(_QWERTY), _______, _______, _______, KC_BSPC, KC_BSPC, _______, _______, _______, TO(_QWERTY), X_____X \
+  _______, X_____X,   X_____X,   X_____X,   X_____X,  X_____X,  X_____X,  X_____X, X_____X, X_____X, X_____X, FIREY_RETURN, \
+  X_____X, TO(_QWERTY), _______, _______, _______, KC_BSPC, KC_BSPC, _______, _______, _______, TO(_QWERTY), RESET \
 ),
 
 
--- keyboards/niu_mini/keymaps/xtonhasvim/keymap.c
+++ keyboards/niu_mini/keymaps/xtonhasvim/keymap.c
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  *
  * - Ctrl acts as Esc when tapped.
- * - Holding A or ; switches to movement layer.
+ * - Holding ; switches to movement layer.
  * - Raise and Lower are one-shot layers.
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,           KC_W,    KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,              KC_BSPC},
-  {LCTL_T(KC_ESC), LT(_MOVE,KC_A), KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    LT(_MOVE,KC_SCLN), KC_QUOT},
-  {KC_LSFT, KC_Z,           KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,   RSFT_T(KC_ENT) },
+  {LCTL_T(KC_ESC), KC_A, KC_S,    KC_D,    KC_F,          KC_G,    KC_H,    KC_J,          KC_K,    KC_L,    LT(_MOVE,KC_SCLN), KC_QUOT},
+  {KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,   RSFT_T(KC_ENT) },
   {LSFT(KC_LALT), TG(_MOUSE),  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC, RAISE,   KC_LGUI, KC_LALT, X_____X, VIM_START }
 },
 
