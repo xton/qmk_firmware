6cc937552a5901f4cf5c3e7bf85613d23cfef403
Xton <cdewan@apple.com>
2018-11-03 22:45:14 -0700

    hahaha. it kinda works!

--- keyboards/handwired/baller/config.h
+++ keyboards/handwired/baller/config.h
@@ -72,4 +72,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+
+// this thing doesn't use mousekeys quite like qmk expects...
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_INTERVAL          1
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       0
+
+
 #endif
--- keyboards/handwired/baller/keymaps/default/keymap.c
+++ keyboards/handwired/baller/keymaps/default/keymap.c
@@ -1,17 +1,68 @@
 #include "baller.h"
 #include <print.h>
+#include <mousekey.h>
+
+static report_mouse_t mouse_report = {};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* {{ KC_A, KC_B, KC_C, KC_D }} */
-    {{ KC_A}}, {{ KC_B}},{{ KC_C}},{{ KC_D }}
+    {{ KC_MS_BTN1 }}
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	// up or down, just send one key event	
-	xprintf("oh hai %d\n", keycode);
-	/* print(keycode); */
-	/* print(record->event.pressed); */
-	register_code(keycode);
-	unregister_code(keycode);
-	return false;
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/* 	// up or down, just send one key event */	
+/* 	xprintf("oh hai %d\n", keycode); */
+/* 	/1* print(keycode); *1/ */
+/* 	/1* print(record->event.pressed); *1/ */
+/* 	register_code(keycode); */
+/* 	unregister_code(keycode); */
+/* 	return false; */
+/* } */
+
+
+#define pin_count 4
+uint16_t pins_were[pin_count] = { 0, 0, 0, 0 };
+uint16_t pins[pin_count] = { C6, D4, D5, D1 };
+uint16_t pins_kc[pin_count] = { KC_MS_UP, KC_MS_DOWN, KC_MS_LEFT, KC_MS_RIGHT };
+
+void matrix_init_user() {
+	for(int i = 0; i < pin_count; i++){
+		setPinInputHigh(pins[i]);
+	}
+
+}
+
+void matrix_scan_user() {
+	for(int i = 0; i < pin_count; i++){
+		uint16_t on = readPin(pins[i]);
+		if(on != pins_were[i]) {
+			xprintf("I changed: %d - %d\n",i, on);
+			/* register_code(pins_kc[i]); */
+			/* unregister_code(pins_kc[i]); */
+			switch(i){
+				case 0: 
+					// up
+					 mouse_report.y = -10;
+					 break;
+				case 1: 
+					// up
+					 mouse_report.y = 10;
+					 break;
+				case 2: 
+					// up
+					 mouse_report.x = -10;
+					 break;
+				case 3: 
+					// up
+					 mouse_report.x = 10;
+					 break;
+			}
+			host_mouse_send(&mouse_report);
+			mouse_report.x = 0;
+			mouse_report.y = 0;
+			host_mouse_send(&mouse_report);
+		}
+		pins_were[i] = on;
+
+	}
 }
