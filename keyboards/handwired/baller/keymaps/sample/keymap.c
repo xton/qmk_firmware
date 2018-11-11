5d30e0984f16420ea31561094a2d2fb6d1cc9d04
Xton <cdewan@apple.com>
2018-11-10 22:06:24 -0800

    IT WORKS

--- keyboards/handwired/baller/keymaps/sample/keymap.c
+++ keyboards/handwired/baller/keymaps/sample/keymap.c
@@ -2,9 +2,10 @@
 #include <print.h>
 #include <mousekey.h>
 #include <util/delay.h>
+#include <string.h>
 #define delay(us)  _delay_us(us) 
 
-/* static report_mouse_t mouse_report = {}; */
+static report_mouse_t mouse_report = {};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* {{ KC_A, KC_B, KC_C, KC_D }} */
@@ -27,10 +28,11 @@ uint16_t pins_were[pin_count] = { 0, 0, 0, 0 };
 uint16_t pins[pin_count] = { D1, D0, D4, C6 };
 uint16_t pins_kc[pin_count] = { KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT };
 #define mouse_step 20
-#define ipin_count 2
-uint16_t ipins[ipin_count] = { B5, B7 };
-uint16_t ipins_were[ipin_count] = { B5, B7 };
-uint32_t deltas[ipin_count] = { 0, 0};
+#define ipin_count 4
+uint16_t ipins[ipin_count] = { B5, B7, B6, B0 };
+uint16_t ipins_were[ipin_count] = {0};
+uint32_t deltas[ipin_count] = {0};
+bool has_moved = false;
 
 void matrix_init_user() {
 	for(int i = 0; i < pin_count; i++){
@@ -39,7 +41,7 @@ void matrix_init_user() {
 	// enable interrupts
 	SREG |= (0x1 << 7);
 	// mask out just B5, B7
-	PCMSK0 |= ((1 << PCINT7) | (1 << PCINT5)); 
+	PCMSK0 |= ((1 << PCINT7) | (1 << PCINT5) | (1 << PCINT6) | (1 << PCINT0)); 
 	// turn on PC int
 	PCICR |= (1 << PCIE0);
 
@@ -47,44 +49,75 @@ void matrix_init_user() {
 #define TAP(kc) do { register_code(kc); unregister_code(kc); } while (0)
 
 void matrix_scan_user() {
-	for(int i = 0; i < pin_count; i++){
-		uint16_t on = readPin(pins[i]);
-		if(on != pins_were[i]) {
-#if 0
-			/* xprintf("I changed: %d - %d\n",i, on); */
-			/* register_code(pins_kc[i]); */
-			/* unregister_code(pins_kc[i]); */
-			switch(i){
-				case 0: 
-					// up
-					 mouse_report.y = -mouse_step;
-					 break;
-				case 1: 
-					// up
-					 mouse_report.y = mouse_step;
-					 break;
-				case 2: 
-					// up
-					 mouse_report.x = -mouse_step;
-					 break;
-				case 3: 
-					// up
-					 mouse_report.x = mouse_step;
-					 break;
-			}
-			host_mouse_send(&mouse_report);
-			mouse_report.x = 0;
-			mouse_report.y = 0;
-			host_mouse_send(&mouse_report);
-#else 
-			xprintf("YAY: %d\n",i);
-			xprintf("deltas: %d %d\n",deltas[0], deltas[1]);
-			/* TAP(pins_kc[i]); */
-#endif
-		}
-		pins_were[i] = on;
-
+	if(has_moved){
+		mouse_report.y += mouse_step*deltas[2];
+		mouse_report.y -= mouse_step*deltas[1];
+		mouse_report.x += mouse_step*deltas[3];
+		mouse_report.x -= mouse_step*deltas[0];
+		host_mouse_send(&mouse_report);
+		memset(&deltas, 0, sizeof(*deltas)*ipin_count);
+		mouse_report.x = 0;
+		mouse_report.y = 0;
+		has_moved = false;
 	}
+	/* case 0: */ 
+	/* 	// up */
+	/* 	 mouse_report.y = -mouse_step; */
+	/* 	 break; */
+	/* case 1: */ 
+	/* 	// up */
+	/* 	 mouse_report.y = mouse_step; */
+	/* 	 break; */
+	/* case 2: */ 
+	/* 	// up */
+	/* 	 mouse_report.x = -mouse_step; */
+	/* 	 break; */
+	/* case 3: */ 
+	/* 	// up */
+	/* 	 mouse_report.x = mouse_step; */
+	/* 	 break; */
+	/* for(int i = 1; i < ipin_count; i++){ */ 
+		
+	/* } */
+	/* for(int i = 0; i < pin_count; i++){ */
+	/* 	uint16_t on = readPin(pins[i]); */
+	/* 	if(on != pins_were[i]) { */
+/* #if 1 */
+	/* 		/1* xprintf("I changed: %d - %d\n",i, on); *1/ */
+	/* 		/1* register_code(pins_kc[i]); *1/ */
+	/* 		/1* unregister_code(pins_kc[i]); *1/ */
+	/* 		switch(i){ */
+	/* 			case 0: */ 
+	/* 				// up */
+	/* 				 mouse_report.y = -mouse_step; */
+	/* 				 break; */
+	/* 			case 1: */ 
+	/* 				// up */
+	/* 				 mouse_report.y = mouse_step; */
+	/* 				 break; */
+	/* 			case 2: */ 
+	/* 				// up */
+	/* 				 mouse_report.x = -mouse_step; */
+	/* 				 break; */
+	/* 			case 3: */ 
+	/* 				// up */
+	/* 				 mouse_report.x = mouse_step; */
+	/* 				 break; */
+	/* 		} */
+	/* 		host_mouse_send(&mouse_report); */
+	/* 		mouse_report.x = 0; */
+	/* 		mouse_report.y = 0; */
+	/* 		host_mouse_send(&mouse_report); */
+/* #else */ 
+	/* 		xprintf("YAY: %d\n",i); */
+	/* 		xprintf("delta0: %d\n",deltas[0]); */
+	/* 		xprintf("delta1: %d\n",deltas[1]); */
+	/* 		/1* TAP(pins_kc[i]); *1/ */
+/* #endif */
+	/* 	} */
+	/* 	pins_were[i] = on; */
+
+	/* } */
 	/* xprintf("%d,%d,%d,%d\n",pins[0],pins[1],pins[2],pins[3]); */
 	/* delay(1000); */
 }
@@ -97,6 +130,7 @@ ISR(PCINT0_vect){
 	if(on != ipins_were[i]) {
 		  xprintf("OH SHIT - %d -> %d\n",i, deltas[i]);
 		deltas[i]++;	
+		has_moved=true;
 	}
 	ipins_were[i] = on;
 
