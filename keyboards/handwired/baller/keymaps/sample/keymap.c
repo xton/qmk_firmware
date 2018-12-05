65dc583cf72a2954a5bec95de9fef81cca7e0f4b
Xton <cdewan@apple.com>
2018-12-04 22:59:23 -0800

    deadcode stripping

--- keyboards/handwired/baller/keymaps/sample/keymap.c
+++ keyboards/handwired/baller/keymaps/sample/keymap.c
@@ -1,11 +1,4 @@
 #include "baller.h"
-/* #include <print.h> */
-/* #include <mousekey.h> */
-/* #include <util/delay.h> */
-/* #include <string.h> */
-/* #define delay(us)  _delay_us(us) */ 
-
-/* static report_mouse_t mouse_report = {}; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* {{ KC_A, KC_B, KC_C, KC_D }} */
@@ -13,69 +6,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = {{ KC_NO, KC_NO, KC_NO, KC_NO }}
 };
 
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/* 	// up or down, just send one key event */	
-/* 	xprintf("oh hai %d\n", keycode); */
-/* 	/1* print(keycode); *1/ */
-/* 	/1* print(record->event.pressed); *1/ */
-/* 	register_code(keycode); */
-/* 	unregister_code(keycode); */
-/* 	return false; */
-/* } */
 
 uint8_t vim_cmd_layer(void) { return 1; }
 
-/* #define pin_count 4 */
-/* uint16_t pins_were[pin_count] = { 0, 0, 0, 0 }; */
-/* uint16_t pins[pin_count] = { D1, D0, D4, C6 }; */
-/* uint16_t pins_kc[pin_count] = { KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT }; */
-/* #define mouse_step 20 */
-/* #define ipin_count 4 */
-/* uint16_t ipins[ipin_count] = { B5, B7, B6, B0 }; */
-/* uint16_t ipins_were[ipin_count] = {0}; */
-/* uint32_t deltas[ipin_count] = {0}; */
-/* bool has_moved = false; */
-
-/* void matrix_init_keymap(void) { */
-/* 	for(int i = 0; i < pin_count; i++){ */
-/* 		setPinInputHigh(pins[i]); */
-/* 	} */
-/* 	// enable interrupts */
-/* 	SREG |= (0x1 << 7); */
-/* 	// mask out just our target pins */
-/* 	PCMSK0 |= ((1 << PCINT7) | (1 << PCINT5) | (1 << PCINT6) | (1 << PCINT0)); */ 
-/* 	// turn on PC int */
-/* 	PCICR |= (1 << PCIE0); */
-
-/* } */
-/* #define TAP(kc) do { register_code(kc); unregister_code(kc); } while (0) */
-
-/* void matrix_scan_keymap(void) { */
-/* 	if(has_moved){ */
-/* 		mouse_report.y += mouse_step*deltas[2]; */
-/* 		mouse_report.y -= mouse_step*deltas[1]; */
-/* 		mouse_report.x += mouse_step*deltas[3]; */
-/* 		mouse_report.x -= mouse_step*deltas[0]; */
-/* 		host_mouse_send(&mouse_report); */
-/* 		memset(&deltas, 0, sizeof(*deltas)*ipin_count); */
-/* 		mouse_report.x = 0; */
-/* 		mouse_report.y = 0; */
-/* 		has_moved = false; */
-/* 	} */
-/* } */
-
-
-
-/* ISR(PCINT0_vect){ */
-/*   for(int i = 0; i < ipin_count; i++){ */ 
-/* 	uint16_t on = readPin(ipins[i]); */
-/* 	if(on != ipins_were[i]) { */
-/* 		  /1* xprintf("OH SHIT - %d -> %d\n",i, deltas[i]); *1/ */
-/* 		deltas[i]++; */	
-/* 		has_moved=true; */
-/* 	} */
-/* 	ipins_were[i] = on; */
-
-/*   } */
-
-/* } */
