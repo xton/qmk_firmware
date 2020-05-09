705ab797a15221a33efcb6e59d3c1d9cda22272c
Xton <cdewan@apple.com>
2020-05-09 13:17:01 -0700

    only the red light

--- keyboards/handwired/sharling/keymaps/default/keymap.c
+++ keyboards/handwired/sharling/keymaps/default/keymap.c
@@ -97,28 +97,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 16, HSV_RED}       // Light 4 LEDs, starting with LED 6
-);
-// Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 16, HSV_CYAN}
+    {0, 16, HSV_RED}       // shouty mode on
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_capslock_layer,
-    my_layer1_layer    // Overrides caps lock layer
+    my_capslock_layer
 );
 
 void keyboard_post_init_user(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Both layers will light up if both kb layers are active
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    return state;
 }
 
 bool led_update_user(led_t led_state) {
